e4ef2873c02e3b23160f72126dc4d1b63b12daf5
seattleGr3y <srmagill@outlook.com>
2017-09-07 19:14:01 -0700

    Add files via upload

---
+++ BlackHolePlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+
+    private int count;
+    public Text countText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+    // Update will contain the code called before rendering a frame
+    // this is where most of the game code will go
+    void Update()
+    {
+
+    }
+
+    // FixedUpdate is called before any physics calculations are made
+    // this is where the physics code will go
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            if (count.Equals(11))
+            {
+                countText.text = "YOU WON THE GAME!!!!";
+            }
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
